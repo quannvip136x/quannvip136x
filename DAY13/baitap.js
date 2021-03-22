1c5f35d55a68ea109b5b1fd603dcbad48d425e37
Shadow <hoangquanx98x@gmail.com>
2021-03-22 08:33:24 +0700

    as

---
+++ DAY13/baitap.js
@@ -0,0 +1,89 @@
+
+
+// bài 2
+function drawRectangle(m, n) {
+    if (m <= 0 || n <= 0 || Number.isInteger(m) === false || Number.isInteger(n) === false) {
+        document.write("Nhập sai rồi dữ liệu");
+        return;
+    }
+    let str = "";
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < m; j++) {
+            str += "&#32;*"
+        }
+        str += "<br/>"
+    }
+    document.write(str)
+}
+drawRectangle(4, 5)
+// bài 3
+function drawEmptyRectangle(m, n) {
+    document.writeln("<br>")
+    if (m <= 0 || n <= 0 || Number.isInteger(m) === false || Number.isInteger(n) === false) {
+        document.write("Nhập sai rồi dữ liệu");
+        return;
+    }
+    let str = "";
+    for (let x = 0; x < m; x++) {
+        str += "* "
+    }
+    for (let i = 1; i < n - 2; i++) {
+        str += "<br>"
+        for (let j = 0; j < m; j++) {
+
+            if (j === 0 || j === m - 1) {
+                str += "* ";
+            } else {
+                str += "* ".fontcolor("white");
+            }
+        }
+    }
+    str += "<br>"
+    for (let x = 0; x < m; x++) {
+        str += "* ";
+    }
+    document.write(str);
+}
+drawEmptyRectangle(4, 5)
+document.write("<br>")
+// bai 4
+function veTamGiacVuong(m) {
+    document.write("<br>")
+    if (m <= 0 || Number.isInteger(m) === false) {
+        document.write("Nhập sai rồi dữ liệu ");
+        return;
+    }
+    let str = "";
+    for (let i = 0; i < m; i++) {
+        str += "<br>"
+        for (let j = 0; j <= i; j++) {
+
+            str += "* ";
+        }
+    }
+    str += "<br>"
+    document.write(str)
+}
+veTamGiacVuong(5)
+// bai 5
+document.write("<br>")
+function veTamGiacCan(m) {
+    document.write("<br>")
+    if (m <= 0 || Number.isInteger(m) === false) {
+        document.write("Nhập sai rồi dữ liệu");
+        return;
+    }
+    let str = "";
+    for (let i = 1; i <= m; i++) {
+        str += "<br>"
+        for (let j = 1; j < i; j++) {
+            str += "* ".fontcolor("white")
+        }
+        for (j = 0; j < (m * 2 - (2 * i - 1)); j++) {
+            str += "* "
+        }
+    }
+    str += "<br>"
+    document.write(str)
+}
+veTamGiacCan(4)
---
+++ DAY13/baitapvn.js
@@ -0,0 +1,98 @@
+
+function bai1(a, b) {
+    let snt = 0;
+    for (let i = a; i <= b; i++) {
+        let test = 1;
+        for (let j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                test = 0;
+
+            }
+        }
+        if (test === 1) {
+            snt++;
+            console.log(i + " : là số Nguyên tố");
+        }
+    }
+    console.log("Số lượng số nguyên tố có trong đoạn ab là", snt)
+}
+
+function chenso(arr) {
+    let max = arr[0];
+    let dem = 0;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+            dem++;
+        }
+    }
+
+    arr.splice(dem + 1, 0, '100')
+    console.log(arr)
+
+}
+
+
+function tangDan(arr) {
+    let a = arr.push('4')
+
+    for (i = 0; i < arr.length; i++) {
+        for (j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                k = arr[i];
+                arr[i] = arr[j];
+                arr[j] = k;
+            }
+        }
+        console.log(arr[i])
+    }
+
+}
+tangDan([1, 5, 3, 9, 6, 8])
+
+function soDoiXung(value) {
+    let soCuoi;
+    let soDao = 0;
+    let tmp = value;
+    while (tmp != 0) {
+        soCuoi = tmp % 10;
+        soDao = (soDao * 10) + soCuoi;
+        tmp = Math.floor(tmp / 10);
+    }
+    if (soDao == value) {
+        console.log("Là đối xứng")
+    } else {
+        console.log("Không đối xứng")
+    }
+}
+
+function giaCuocTaxi() {
+    soKm = prompt("Nhập số km")
+    let b = prompt("Thời gian chờ bạn sau khi gọi xe lưu ý tính bằng phút")
+    let giaCuoc = 0;
+    if (soKm < 0 || b < 0) {
+        return `Km hoặc thời gian sai mời nhập lại`
+    }
+    if (soKm < 1) {
+        giaCuoc = 9000
+        console.log("Số tiền cần trả là:", giaCuoc)
+    } else if (1 < soKm && soKm < 31 && b == 0) {
+        giaCuoc = (soKm * 11000) + 9000
+        console.log("Số tiền cần trả là:", giaCuoc)
+        console.log("Thời gian chờ là 0")
+    } else if (soKm > 30 && b == 0) {
+        giaCuoc = 9000 + 330000 + ((soKm - 30) * 9500)
+        console.log("Số tiền cần trả là:", giaCuoc)
+        console.log("Thời gian chờ là 0")
+    }
+    if (1 < soKm && soKm && b > 1) {
+        giaCuoc = 400 * b + (soKm * 11000) + 9000
+        console.log("Số tiền cần phải trả sau khi chờ:", giaCuoc)
+        console.log("Thời gian chờ là", b + " Phút")
+    }
+    if (soKm > 30 && b > 1) {
+        giaCuoc = 400 * b + (soKm * 11000) + 9000
+        console.log("Số tiền cần phải trả sau khi chờ", giaCuoc)
+        console.log("Thời gian chờ là", b + " Phút")
+    }
+}
