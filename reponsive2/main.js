cd7f2de5a02bc4569a1b5684966920b3a1fba3d1
Shadow <hoangquanx98x@gmail.com>
2021-04-16 09:37:23 +0700

    as

--- reponsive2/main.js
+++ reponsive2/main.js
@@ -3,3 +3,8 @@
         function menu() {
             $('#sub').toggleClass('show');
         }
+        if (window.matchMedia && window.matchMedia('(max-width: 500px)').matches) {
+            // Code xử lý khi màn hình có chiều rộng nhỏ hơn hoặc bằng 500px
+            
+        }
+        
