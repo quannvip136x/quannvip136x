b13969ebee35773bf73483a12a2a2cc6caf51941
Shadow <hoangquanx98x@gmail.com>
2021-04-23 11:16:48 +0700

    as

---
+++ DreamCar/js/main.js
@@ -0,0 +1,18 @@
+function change() {
+    $('#navbarNav').toggleClass('show');
+    $('.sign-up').toggleClass('none')
+
+}
+if (window.matchMedia && window.matchMedia('(max-width: 900px)').matches) {
+    // Code xử lý khi màn hình có chiều rộng nhỏ hơn hoặc bằng 500px
+    
+}
+function footer() {
+    $('#navbarNav1').toggleClass('show1');
+}
+if (window.matchMedia && window.matchMedia('(max-width: 900px)').matches) {
+    // Code xử lý khi màn hình có chiều rộng nhỏ hơn hoặc bằng 500px
+    
+}
+
+
