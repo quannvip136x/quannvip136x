e5200360c573eceae1feb55ebe1c0a0e173c7f3f
Shadow <hoangquanx98x@gmail.com>
2021-07-30 19:41:00 +0700

    sss

--- FigureStore/main.js
+++ FigureStore/main.js
@@ -7,3 +7,11 @@ function hide() {
     $(".cart-items").removeClass('show');
     $(".container").removeClass('opacity');
 }
+function ShowMenu() {
+    $(".menu-ipad").toggle('show');
+}
+function success() {
+    alert('Cảm ơn quý khách đã mua hàng');
+    console.log('aaa')
+    
+}
